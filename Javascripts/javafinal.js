e34693e6873047bde7fd87192fcd661ed83d4af0
LEE <65538262+Misscrown3827@users.noreply.github.com>
2020-06-12 16:12:28 +0900

    Add files via upload

---
+++ Javascripts/javafinal.js
@@ -0,0 +1,10 @@
+console.log(100+10);   // 110
+console.log(100+'10'); // 10010
+console.log("\'안녕\'");
+
+
+console.log(10 == 10);
+console.log(10 == "10");
+
+console.log(10 === "10");
+console.log(10 === 10);
---
+++ Javascripts/javasc2.js
@@ -0,0 +1,54 @@
+ // 자바 문제 
+ /*var a = 10;
+ var b = 2;
+ var j = 0;
+ for (i = 1; i < 5; i += 2) {
+     a += i
+     j++
+     console.log(j)
+ }
+
+ console.log(a + b);*/
+
+
+ /* console.log("test");
+  var test;
+  console.log(test);*/
+
+ /*var rarar = 10;
+ rara = rara+1;
+ rara += 1;
+ rara++;
+ ++rara;// 14
+ rara;
+
+ console.log(rara++);
+ //rara = 15
+
+ console.log(++rara);
+
+ rara++;
+ ++rara;
+
+ console.log(rara);*/
+
+ /*console.log(100+10); // 110
+ console.log(100-10); // 90
+ console.log(100*10); //1000
+ console.log(100/10); // 10
+ console.log(""); // 가독성을 위해 1칸 뜀
+ console.log("100"+10); // 10010
+ console.log("100"-10); // 90
+ console.log("100"*10); // 1000
+ console.log("100"/10); // 10
+ console.log("");
+ console.log(100+"10"); // 10010
+ console.log(100-"10"); // 90
+ console.log(100*"10"); // 1000
+ console.log(100/"10"); // 10
+ console.log("");
+ console.log("100"+"10"); // 10010
+ console.log("100"-"10"); // 90
+ console.log("100"*"10"); // 1000
+ console.log("100"/"10"); //
+ console.log("");*/
